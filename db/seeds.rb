1ca88289a659fcea2d6accf45c7ee3c2ca0f4a54
vishal-sns <vishal.bharti@snstech.com>
2014-11-21 19:22:28 +0530

    Added seed data for speciality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#---CREATE HOSPITALS
 Hospital.create(name: 'All India Institute of Medical sciences Delhi',address1:'Ansari Nagar East, Gautam Nagar, New Delhi',city:'New Delhi',state:'Delhi',multispeciality:true,branch:'Delhi')
 Hospital.create(name: 'Apollo Hospital',address1:'Ansari Nagar East, Mettukuppam, Chennai',city:'Chennai',state:'Chennai',multispeciality:true,branch:'Chennai')
-Doctor.create(first_name: 'Vishal',last_name: 'Bharti',age:'25',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4)
-Doctor.create(first_name: 'Venkatesh',last_name: 'Reddy',age:'35',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4)
-Doctor.create(first_name: 'Ajay',last_name: 'Kumar',age:'45',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4)
-Doctor.create(first_name: 'K.K.',last_name: 'Sinha',age:'52',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4)
+
+#---CREATE DOCTORS
+Doctor.create([{first_name: 'Vishal',last_name: 'Bharti',age:'25',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4},
+{first_name: 'Venkatesh',last_name: 'Reddy',age:'35',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4},
+{first_name: 'Ajay',last_name: 'Kumar',age:'45',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4},
+{first_name: 'K.K.',last_name: 'Sinha',age:'52',sex:true,qualification:'MBBS',speciality:'Dentist',rating:4}]
+
+#---CREATE SPECIALITIES
+Speciality.create([{name: 'Dentist'}, { name: 'Dermatologist'}, { name: 'Ayurveda'}, { name: 'Homeopathic'}, { name: 'Gyenocologists'}, { name: 'Psychologist'}, { name: 'Psychiatrist'}, 
+  { name: 'Cardiologist'}, { name: 'Physiotherapist'}, { name: 'Physician'}, { name: 'Neurology'}, { name: 'Nephrology'}]
+
+
+
